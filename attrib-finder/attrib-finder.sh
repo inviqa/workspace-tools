e6382807b30960af0e8ed94f44cd471bbe36736e
MarcoMC <marco@marcomc.com>
2023-02-07 19:47:31 -0400

    Add support for command parameters

--- attrib-finder/attrib-finder.sh
+++ attrib-finder/attrib-finder.sh
@@ -1,16 +1,81 @@
 #!/usr/bin/env bash
-DEBUG=0 #0,1,2
-SHOW_ENCRYPTED_ONLY=true
-ATTRIBUTES=()
 
-ws_file="${1:-workspace.yml}"
+function load_parameters {
+    tmp="${0%.*}"
+    ME=${tmp##*/}
+    DEBUG=0 #0,1,2
 
-if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | WS_FILE: ${ws_file}"; fi
+    ATTRIBUTES=()
+    WS_FILE="workspace.yml"
+    SHOW_ENCRYPTED_ONLY=false
+    
+    read_parameters "${@}"
+    if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | WS_FILE: ${WS_FILE}"; fi
+}
+
+function print_usage {
+  printf  "usage: %s [options] \n" "${ME}"
+  printf  "\noptions:"
+  USAGE="
+      -d|--debug <level>                0: show only command errors
+                                        1: show all the attributes that are read and their type
+                                        2: show all computed steps output
+      -e|--encrypted                    Print list of encrypted attributes only
+      -h|--help                         Print this help  
+      -q|--quiet                        Equal to --debug 0
+      -w|--workspace-file               Path to the Workspace file where the encrypted secrets are stored (defaults to workspace.yml)
+"
+echo "${USAGE}"
+}
+
+function validate_argument {
+  ARGUMENT="${1}"
+  if [[ -z "${ARGUMENT}" || "${ARGUMENT}" == "-*" ]]; then
+    echo "(e) | Invalid or missing argument ${1}"
+    print_usage
+    exit 1
+  fi
+}
+
+function read_parameters {
+  if [[ -n "${1}" ]]; then
+    case "${1}" in
+      -d|--debug)
+        validate_argument "${2}"
+        DEBUG="${2}"
+        shift 2
+        ;;
+      -e|--encrypted)
+        SHOW_ENCRYPTED_ONLY=true
+        shift 1
+        ;;
+      -h|--help)
+        print_usage
+        exit 0
+        ;;
+      -q|--quiet)
+        DEBUG=0
+        shift 1
+        ;;
+      -w|--workspace-file)
+        validate_argument "${2}"
+        WS_FILE="${2}"
+        shift 2
+        ;;
+      *)
+        echo "(e) | Invalid option: ${1}"
+        print_usage
+        exit 1
+        ;;
+    esac
+    read_parameters "${@}"
+  fi
+}
 
 function get_inline_attributes {
     START_WITH='attribute('
     # shellcheck disable=SC2207
-    INLINE_ATTRIBUTES=( $(yq "${ws_file}" -o json | jq  -r 'to_entries[] | select(.key | startswith("'"${START_WITH}"'")) | .key' || true  ) )
+    INLINE_ATTRIBUTES=( $(yq "${WS_FILE}" -o json | jq  -r 'to_entries[] | select(.key | startswith("'"${START_WITH}"'")) | .key' || true  ) )
     if [[ ${#INLINE_ATTRIBUTES[*]} -ge 1 ]]; then
         if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | INLINE_ATTRIBUTES: ${INLINE_ATTRIBUTES[*]}"; fi
         if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | INLINE_ATTR_#: ${#INLINE_ATTRIBUTES[*]}"; fi
@@ -23,7 +88,7 @@ function get_inline_attributes {
                 get_inline_children_attributes "${ATTRIBUTE}"
             else
                 if [[ ${SHOW_ENCRYPTED_ONLY} == true ]];then 
-                    DECRYPT=$(grep "${ATTRIBUTE}" < "${ws_file}" | grep "decrypt" || true)
+                    DECRYPT=$(grep "${ATTRIBUTE}" < "${WS_FILE}" | grep "decrypt" || true)
                     if [[ -n "${DECRYPT}" ]];then
                         if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | INLINE: ${ATTRIBUTE}"; fi
                         if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | DECRYPT: ${DECRYPT}"; fi
@@ -42,7 +107,7 @@ function get_inline_children_attributes {
     PARENT_ATRIBUTE="${1}"
     if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | PARENT: ${PARENT_ATRIBUTE}"; fi
     # shellcheck disable=SC2207
-    CHILDREN=( $(yq "${ws_file}" -o json | jq  -r 'to_entries[] | select(.key | startswith("attribute('\'"${PARENT_ATRIBUTE}"\'')")) | .value' | jq -r 'paths | map(.|tostring) | join(".")' || true  ) )
+    CHILDREN=( $(yq "${WS_FILE}" -o json | jq  -r 'to_entries[] | select(.key | startswith("attribute('\'"${PARENT_ATRIBUTE}"\'')")) | .value' | jq -r 'paths | map(.|tostring) | join(".")' || true  ) )
     if [[ ${#CHILDREN[*]} -ge 1 ]]; then
         if [[ ${DEBUG} -ge 2 ]]; then  echo "(d) | CHILDREN_#: ${#CHILDREN[*]}"; fi
         for (( CHILD=0; CHILD<${#CHILDREN[*]}; CHILD++ ));
@@ -54,7 +119,7 @@ function get_inline_children_attributes {
                     # Skip looking for decrypt if the child name is a number, because it means it's a list not an "array" of attributes
                     if ! [[ "${CHILD_NAME}" =~ ^[0-9]+$ ]] ; then
                         if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | CHILD: ${CHILD_NAME}"; fi
-                        DECRYPT=$(grep "${CHILDREN[${CHILD}]}" < "${ws_file}" | grep "decrypt" || true)
+                        DECRYPT=$(grep "${CHILDREN[${CHILD}]}" < "${WS_FILE}" | grep "decrypt" || true)
                         if [[ -n "${DECRYPT}" ]];then
                             if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | CHILD${CHILD}: ${CHILD_ATTRIBUTE}"; fi
                             if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | DECRYPT: ${DECRYPT}"; fi
@@ -72,7 +137,7 @@ function get_inline_children_attributes {
 
 function get_nested_attributes {
     # shellcheck disable=SC2207
-    NESTED_ATTRIBUTES=( $(yq '.attributes' "${ws_file}" -o json | jq -r 'paths | map(.|tostring) | join(".")' || true  ))
+    NESTED_ATTRIBUTES=( $(yq '.attributes' "${WS_FILE}" -o json | jq -r 'paths | map(.|tostring) | join(".")' || true  ))
     if [[ ${#NESTED_ATTRIBUTES[*]} -ge 1 ]]; then
         if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | NESTED_ATTRIBUTES: ${NESTED_ATTRIBUTES[*]}"; fi
         if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | NESTED_ATTRIBUTES_#: ${#NESTED_ATTRIBUTES[*]}"; fi
@@ -85,7 +150,7 @@ function get_nested_attributes {
                     # Skip looking for decrypt if the child name is a number, because it means it's a list not an "array" of attributes
                     if ! [[ ${ATTRIBUTE_NAME} =~ ^[0-9]+$ ]] ; then
                         # Skip looking for decrypt if the child name is a number, because it means it's a list not an "array" of attributes
-                        DECRYPT=$(grep "${ATTRIBUTE_NAME}" < "${ws_file}" | grep "decrypt" || true)
+                        DECRYPT=$(grep "${ATTRIBUTE_NAME}" < "${WS_FILE}" | grep "decrypt" || true)
                         if [[ -n "${DECRYPT}" ]];then
                             if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | NESTED: ${ATTRIBUTE}"; fi
                             if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | DECRYPT: ${DECRYPT}"; fi
@@ -117,6 +182,7 @@ function list_attributes {
     fi
 }
 
+load_parameters "${@}"
 get_inline_attributes
 get_nested_attributes
 
