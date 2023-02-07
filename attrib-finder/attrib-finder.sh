9895903a8ac7f8a34b3da5daf234ac1a0cc4ba10
MarcoMC <marco@marcomc.com>
2023-02-07 19:36:25 -0400

    Add option to print the list of only encrypted attributes

--- attrib-finder/attrib-finder.sh
+++ attrib-finder/attrib-finder.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-DEBUG=1 #0,1,2
+DEBUG=0 #0,1,2
+SHOW_ENCRYPTED_ONLY=true
 ATTRIBUTES=()
 
 ws_file="${1:-workspace.yml}"
@@ -7,11 +8,9 @@ ws_file="${1:-workspace.yml}"
 if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | WS_FILE: ${ws_file}"; fi
 
 function get_inline_attributes {
-
     START_WITH='attribute('
     # shellcheck disable=SC2207
     INLINE_ATTRIBUTES=( $(yq "${ws_file}" -o json | jq  -r 'to_entries[] | select(.key | startswith("'"${START_WITH}"'")) | .key' || true  ) )
-
     if [[ ${#INLINE_ATTRIBUTES[*]} -ge 1 ]]; then
         if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | INLINE_ATTRIBUTES: ${INLINE_ATTRIBUTES[*]}"; fi
         if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | INLINE_ATTR_#: ${#INLINE_ATTRIBUTES[*]}"; fi
@@ -20,11 +19,20 @@ function get_inline_attributes {
             ATTRIBUTE="${INLINE_ATTRIBUTES[${ATTRIB}]}"
             ATTRIBUTE="${ATTRIBUTE##*\(\'}"
             ATTRIBUTE="${ATTRIBUTE%%\'\)}"
-            if is_attribute_array "${ATTRIBUTE}";then
+            if is_attribute_array "${ATTRIBUTE}"; then
                 get_inline_children_attributes "${ATTRIBUTE}"
             else
-                if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | INLINE: ${ATTRIBUTE}"; fi
-                ATTRIBUTES+=("${ATTRIBUTE}")
+                if [[ ${SHOW_ENCRYPTED_ONLY} == true ]];then 
+                    DECRYPT=$(grep "${ATTRIBUTE}" < "${ws_file}" | grep "decrypt" || true)
+                    if [[ -n "${DECRYPT}" ]];then
+                        if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | INLINE: ${ATTRIBUTE}"; fi
+                        if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | DECRYPT: ${DECRYPT}"; fi
+                        ATTRIBUTES+=("${ATTRIBUTE}")
+                    fi
+                else
+                    if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | INLINE: ${ATTRIBUTE}"; fi
+                    ATTRIBUTES+=("${ATTRIBUTE}")
+                fi
             fi
         done
     fi
@@ -39,10 +47,24 @@ function get_inline_children_attributes {
         if [[ ${DEBUG} -ge 2 ]]; then  echo "(d) | CHILDREN_#: ${#CHILDREN[*]}"; fi
         for (( CHILD=0; CHILD<${#CHILDREN[*]}; CHILD++ ));
         do
-            CHILD_ATTRIBUTE="${PARENT_ATRIBUTE}.${CHILDREN[${CHILD}]}"
+            CHILD_NAME="${CHILDREN[${CHILD}]}"
+            CHILD_ATTRIBUTE="${PARENT_ATRIBUTE}.${CHILD_NAME}"
             if ! is_attribute_array "${CHILD_ATTRIBUTE}";then
-                if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | CHILD${CHILD}: ${CHILD_ATTRIBUTE}"; fi
-                ATTRIBUTES+=("${CHILD_ATTRIBUTE}")
+                if [[ ${SHOW_ENCRYPTED_ONLY} == true ]];then
+                    # Skip looking for decrypt if the child name is a number, because it means it's a list not an "array" of attributes
+                    if ! [[ "${CHILD_NAME}" =~ ^[0-9]+$ ]] ; then
+                        if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | CHILD: ${CHILD_NAME}"; fi
+                        DECRYPT=$(grep "${CHILDREN[${CHILD}]}" < "${ws_file}" | grep "decrypt" || true)
+                        if [[ -n "${DECRYPT}" ]];then
+                            if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | CHILD${CHILD}: ${CHILD_ATTRIBUTE}"; fi
+                            if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | DECRYPT: ${DECRYPT}"; fi
+                            ATTRIBUTES+=("${CHILD_ATTRIBUTE}")
+                        fi
+                    fi
+                else
+                    if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | CHILD${CHILD}: ${CHILD_ATTRIBUTE}"; fi
+                    ATTRIBUTES+=("${CHILD_ATTRIBUTE}")
+                fi
             fi
         done
     fi
@@ -58,8 +80,22 @@ function get_nested_attributes {
         do
             ATTRIBUTE="${NESTED_ATTRIBUTES[${ATTRIB}]}"
             if ! is_attribute_array "${ATTRIBUTE}";then
-                if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | NESTED: ${ATTRIBUTE}"; fi
-                ATTRIBUTES+=("${ATTRIBUTE}")
+                if [[ ${SHOW_ENCRYPTED_ONLY} == true ]];then
+                    ATTRIBUTE_NAME="${ATTRIBUTE##*.}"
+                    # Skip looking for decrypt if the child name is a number, because it means it's a list not an "array" of attributes
+                    if ! [[ ${ATTRIBUTE_NAME} =~ ^[0-9]+$ ]] ; then
+                        # Skip looking for decrypt if the child name is a number, because it means it's a list not an "array" of attributes
+                        DECRYPT=$(grep "${ATTRIBUTE_NAME}" < "${ws_file}" | grep "decrypt" || true)
+                        if [[ -n "${DECRYPT}" ]];then
+                            if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | NESTED: ${ATTRIBUTE}"; fi
+                            if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | DECRYPT: ${DECRYPT}"; fi
+                            ATTRIBUTES+=("${ATTRIBUTE}")
+                        fi
+                    fi
+                else
+                    if [[ ${DEBUG} -ge 1 ]]; then echo "(d) | NESTED: ${ATTRIBUTE}"; fi
+                    ATTRIBUTES+=("${ATTRIBUTE}")
+                fi
             fi
         done
     fi
@@ -70,9 +106,23 @@ function is_attribute_array {
     ws config dump --key="${ATTRIBUTE}" | grep -q "array" && return 0 || return 1 || true
 }
 
+function list_attributes {
+    if [[ ${#ATTRIBUTES[*]} -gt 0 ]];then 
+        for (( ATTRIBUTE=0; ATTRIBUTE<${#ATTRIBUTES[*]}; ATTRIBUTE++ ));
+        do
+            echo "${ATTRIBUTES[${ATTRIBUTE}]}"
+        done 
+    else
+        echo "No attributes found"
+    fi
+}
 
 get_inline_attributes
 get_nested_attributes
+
+if [[ ${DEBUG} -eq 0 ]]; then
+    list_attributes
+fi
 
 if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | ALL_ATTR: ${ATTRIBUTES[*]}"; fi
 if [[ ${DEBUG} -ge 2 ]]; then echo "(d) | ALL_ATTR_#: ${#ATTRIBUTES[*]}"; fi
